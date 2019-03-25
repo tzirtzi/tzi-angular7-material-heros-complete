1bae1cdb509584a0c39c5aa669f18d38031f5b51
tzirtzi <tzirtzi@gmail.com>
2019-03-25 21:59:16 +0200

    Flex Reactive on screen size for Dashboard!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -26,6 +26,7 @@ import { DataService } from './data.service';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { Hero2Service } from '../hero-v2.service';
@@ -10,10 +11,18 @@ import { Hero2Service } from '../hero-v2.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  options: FormGroup;
+  isReactiveView: boolean = false;
 
   constructor(
     private heroService: HeroService,
-    private hero2Service: Hero2Service, ) { }
+    private hero2Service: Hero2Service, 
+    private fb: FormBuilder
+    ) {
+      this.options = fb.group({
+      matView: '1'
+    });
+  }
 
   ngOnInit() {
     this.getHeroes();
@@ -22,9 +31,11 @@ export class DashboardComponent implements OnInit {
   getHeroes(): void {
     this.hero2Service.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
-    
   }
 
+  toggleReactiveView() {
+    this.isReactiveView = !this.isReactiveView;
+  }
 }
 
 
